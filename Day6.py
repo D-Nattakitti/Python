d6b9c7d943c28a20a90443a56bb0709e26081c20
Nattakitti Matchawong <160414295+D-Nattakitti@users.noreply.github.com>
2025-10-21 16:16:22 +0700

    1

---
+++ Day6.py
@@ -0,0 +1,14 @@
+# ****** BMI ******
+def bmi_calculator(weight, height):
+    
+    return
+
+weight_float = input("กรุณาน้ำหนักของคุณ: ")
+height_float = input("กรุณากรอกส่วนสูง: ")
+
+weight = float(weight_float)
+weight = float(height_float)
+
+bmi = bmi_calculator
+
+print
