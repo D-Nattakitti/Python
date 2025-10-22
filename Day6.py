9bfdd76af8d23efe2b74096449ad67adf9773bca
Nattakitti Matchawong <160414295+D-Nattakitti@users.noreply.github.com>
2025-10-22 13:08:55 +0700

    Day 6
    Demo V0.1

--- Day6.py
+++ Day6.py
@@ -1,8 +1,34 @@
 # ****** BMI ******
 def bmi_calculator(weight, height):
-    weight_float = input("กรุณาน้ำหนักของคุณ: ")
-    height_float = input("กรุณากรอกส่วนสูง: ")
+    """คำนวณค่า BMI จากน้ำหนัก (kg) และส่วนสูง (cm)"""
+    return weight / (height / 100) ** 2
 
-    return
+def greet(name):
+    print(f"สวัสดี {name} ขอให้วันนี้เป็นวันที่ดี")
 
-bmi = bmi_calculator
+def bmi_status(bmi):
+    # แสดงผลลัพธ์แบบข้อความ 
+    if bmi < 18.5:
+        return "น้ำหนักน้อยกว่ามาตรฐาน"
+    elif bmi < 25:
+        return "น้ำหนักปกติ"
+    elif bmi < 30:
+        return "น้ำหนักเกิน"
+    else:
+        return "อ้วน"
+    
+
+#   ********* Main Program *********
+
+try:
+    name = input("กรุณาใส่ชื่อของคุณ: ")
+    weight = float(input("กรุณาใส่น้ำหนักคุณ (กก.): "))
+    height = float(input("กรุณาใส่ส่วนสูงของคุณ (ซม.): "))
+
+    bmi = bmi_calculator(weight, height)
+    greet(name)
+    print(f"ค่า BMI ของคุณคือ {bmi:.2f}")
+    print(f"อยู๋ในเกณฑ์: {bmi_status(bmi)}")
+
+except ValueError:
+    print("กรุณาใส่ค่าให้ถูกต้อง")
