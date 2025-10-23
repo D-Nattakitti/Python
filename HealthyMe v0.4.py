90a9181e29624d88a819497781a15e84ff3404db
Nattakitti Matchawong <160414295+D-Nattakitti@users.noreply.github.com>
2025-10-24 00:01:27 +0700

    Update HealthyMe v0.4.3

--- HealthyMe v0.4.py
+++ HealthyMe v0.4.py
@@ -1,31 +1,34 @@
 def calculate_bmi(weight, height):
-    bmi = weight / (height/100)**2
-    return bmi
+    return weight / (height / 100) ** 2
 
 def save_to_file(name, weight, height, bmi):
-    file = open("health_log.txt", "a")
-    file.write(f"Name: {name}, Wegiht: {weight}, Height: {height}, BMI: {bmi:.2f}")
-    file.close()
+    with open("health_log.txt", "a") as file:
+        file.write(f"Name: {name}, Weight: {weight}, Height: {height}, BMI: {bmi:.2f}\n")
     print("✅ บันทึกข้อมูลเรียบร้อยแล้ว!")
 
-def read_health_log(name, weight, height, bmi):
-    with open("health_log.txt", "r") as file:
-        content = file.read()
-        print(content)
-    
-
-
+def read_health_log():
+    try:
+        with open("health_log.txt", "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "ยังไม่มีข้อมูลเก่าครับ"
 
 def main():
     print("=== HealthyMe V0.4 ===")
-    print(f"ข้อมูลเก่า: \n{read_health_log(contant)}
+
+    old_data = read_health_log()
+    print("📄 ข้อมูลเก่า:")
+    print(old_data)
+    print("----------------------")
+
     name = input("ชื่อของคุณ: ")
     weight = float(input("น้ำหนัก (kg): "))
     height = float(input("ส่วนสูง (cm): "))
 
     bmi = calculate_bmi(weight, height)
     print(f"ค่า BMI ของคุณคือ {bmi:.2f}")
-    save_to_file(name, weight, height, bmi)
-    
-main()
 
+    save_to_file(name, weight, height, bmi)
+
+main()
